89951c838e75873d2c286ad7d2e8228bcfe72b51
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-02-02 18:27:22 +0530

    implimenting UI

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { AddDataFormComponent } from "./components/add-data-form/add-data-form.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavBarComponent]
+    imports: [RouterOutlet, NavBarComponent, AddDataFormComponent]
 })
 export class AppComponent {
   title = 'frontend';
---
+++ frontend/src/app/components/add-data-form/add-data-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-data-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-data-form.component.html',
+  styleUrl: './add-data-form.component.css'
+})
+export class AddDataFormComponent {
+
+}
